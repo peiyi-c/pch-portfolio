0b73e0dece47cf34db4c59384d812e24a1215909
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 20:03:54 +0200

    add: footer wave

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,17 @@
 import "./index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Wave from "../../assets/wave.svg";
 
 const Footer = () => {
   return (
     <>
-      <footer id="contact" className="contact container">
-        <FontAwesomeIcon icon={faEnvelope} />
+      <footer id="contact" className="contact">
+        <div className="contact__links">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </div>
       </footer>
+      <img className="concat__wave" aria-hidden="true" src={Wave} />
     </>
   );
 };
