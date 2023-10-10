d02d104db8f43098c4342f411ddc1335527e1795
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 18:05:43 +0200

    add: work

---
+++ src/components/Work/index.jsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+const Work = () => {
+  return (
+    <>
+      <div className="works__item">work</div>
+    </>
+  );
+};
+
+export default Work;
--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -1,16 +1,15 @@
 import "./index.scss";
-
+import Work from "../Work";
 const Works = () => {
   return (
     <>
       <section id="works" className="works container">
-        <div className="works__item">work</div>
-        <div className="works__item">work</div>
-        <div className="works__item">work</div>
-        <div className="works__item">work</div>
-        <div className="works__item">work</div>
-        <div className="works__item">work</div>
+        <Work />
+        <Work />
+        <Work />
+        <Work />
       </section>
+      <hr />
     </>
   );
 };
