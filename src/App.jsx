0ca98dbb79564ba195d6d57f745e295d7182e060
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 18:57:31 +0200

    add: skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.scss";
 import Navigation from "./components/Navigation";
 import Intro from "./components/Intro";
 import Works from "./components/Works";
+import Skills from "./components/Skills";
 function App() {
   return (
     <>
       <Navigation />
       <Intro />
       <Works />
+      <Skills />
     </>
   );
 }
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,1 +1,39 @@
 import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faHtml5,
+  faCss3,
+  faSquareJs,
+  faSass,
+  faBootstrap,
+  faNodeJs,
+  faVuejs,
+  faReact,
+  faGit,
+  faNpm,
+  faFigma,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Skills = () => {
+  return (
+    <>
+      <section id="skills" className="skills container">
+        <FontAwesomeIcon className="skills__skill" icon={faHtml5} />
+        <FontAwesomeIcon className="skills__skill" icon={faCss3} />
+        <FontAwesomeIcon className="skills__skill" icon={faSquareJs} />
+        <FontAwesomeIcon className="skills__skill" icon={faSass} />
+        <FontAwesomeIcon className="skills__skill" icon={faBootstrap} />
+        <FontAwesomeIcon className="skills__skill" icon={faNodeJs} />
+        <FontAwesomeIcon className="skills__skill" icon={faVuejs} />
+        <FontAwesomeIcon className="skills__skill" icon={faReact} />
+        <FontAwesomeIcon className="skills__skill" icon={faGit} />
+        <FontAwesomeIcon className="skills__skill" icon={faNpm} />
+        <FontAwesomeIcon className="skills__skill" icon={faFigma} />
+      </section>
+      <hr />
+    </>
+  );
+};
+
+export default Skills;
