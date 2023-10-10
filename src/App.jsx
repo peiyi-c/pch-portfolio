ceeaa89366e86b6f4c56c0136ed9d969e9aa5c65
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 16:36:58 +0200

    add: intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.scss";
 import Navigation from "./components/Navigation";
-
+import Intro from "./components/Intro";
 function App() {
   return (
     <>
       <Navigation />
+      <Intro />
     </>
   );
 }
--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,1 +1,19 @@
 import "./index.scss";
+const Intro = () => {
+  return (
+    <>
+      <section id="intro" className="intro container">
+        <h1>About Me</h1>
+        <p>Here some creative intro text, thank you.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          nihil aperiam, tempore doloremque libero impedit facere unde sint,
+          tenetur amet incidunt, excepturi recusandae doloribus accusantium
+          dolorem odio ipsum optio? Error!
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Intro;
