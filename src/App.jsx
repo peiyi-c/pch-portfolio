287633e1d1c8d114bddd0fab0f5ee31ffe840b09
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 16:34:52 +0200

    add: navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.scss";
 import Navigation from "./components/Navigation";
-import Intro from "./components/Intro";
+
 function App() {
   return (
     <>
       <Navigation />
-      <Intro />
     </>
   );
 }
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,1 +1,50 @@
 import "./index.scss";
+import LogoPCH from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
+
+const Navigation = () => {
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMenu(menu ? false : true);
+  };
+
+  return (
+    <>
+      <nav id="nav" className="nav container">
+        <div className="nav__logo">
+          <HashLink smooth to="/">
+            <img src={LogoPCH} alt="PCH" />
+          </HashLink>
+        </div>
+
+        <div className="nav__bar" onClick={toggleMenu}>
+          <div className="nav__bar__menu">
+            {menu ? (
+              <FontAwesomeIcon icon={faXmark} />
+            ) : (
+              <FontAwesomeIcon icon={faBars} />
+            )}
+          </div>
+          <div className={menu ? "nav__bar__links open" : "nav__bar__links"}>
+            <HashLink smooth to="/#works">
+              Works
+            </HashLink>
+            <HashLink smooth to="/#skills">
+              Skills
+            </HashLink>
+            <HashLink smooth to="/#contact">
+              Contact
+            </HashLink>
+          </div>
+        </div>
+      </nav>
+      <hr />
+    </>
+  );
+};
+
+export default Navigation;
