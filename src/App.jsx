c865c67b4cfec9db36c7c6f6630a20983f6e4f00
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-10 17:34:07 +0200

    add: works

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Navigation from "./components/Navigation";
 import Intro from "./components/Intro";
+import Works from "./components/Works";
 function App() {
   return (
     <>
       <Navigation />
       <Intro />
+      <Works />
     </>
   );
 }
---
+++ src/components/Works/index.jsx
@@ -0,0 +1,18 @@
+import "./index.scss";
+
+const Works = () => {
+  return (
+    <>
+      <section id="works" className="works container">
+        <div className="works__item">work</div>
+        <div className="works__item">work</div>
+        <div className="works__item">work</div>
+        <div className="works__item">work</div>
+        <div className="works__item">work</div>
+        <div className="works__item">work</div>
+      </section>
+    </>
+  );
+};
+
+export default Works;
