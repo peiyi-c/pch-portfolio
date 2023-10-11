60663ffc2ba8f112791b029ace83c91bdf616ac8
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-11 22:46:05 +0200

    add: icon hover effect

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -20,19 +20,49 @@ const Skills = () => {
     <>
       <section id="skills" className="skills container">
         <h1 className="skills__title">Skills</h1>
-        <div className="container">
-          <FontAwesomeIcon className="skills__skill" icon={faHtml5} />
-          <FontAwesomeIcon className="skills__skill" icon={faCss3} />
-          <FontAwesomeIcon className="skills__skill" icon={faSquareJs} />
-          <img className="skills__skill typescript" src={TypeScript} />
-          <FontAwesomeIcon className="skills__skill" icon={faSass} />
-          <FontAwesomeIcon className="skills__skill" icon={faBootstrap} />
-          <FontAwesomeIcon className="skills__skill" icon={faNodeJs} />
-          <FontAwesomeIcon className="skills__skill" icon={faVuejs} />
-          <FontAwesomeIcon className="skills__skill" icon={faReact} />
-          <FontAwesomeIcon className="skills__skill" icon={faGit} />
-          <FontAwesomeIcon className="skills__skill" icon={faNpm} />
-          <FontAwesomeIcon className="skills__skill" icon={faFigma} />
+        <div className="container skills__list">
+          <span className="skills__skill" data-name="HTML">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faHtml5} />
+          </span>
+          <span className="skills__skill" data-name="CSS">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faCss3} />
+          </span>
+          <span className="skills__skill" data-name="Scss">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faSass} />
+          </span>
+          <span className="skills__skill" data-name="Bootstrap">
+            <FontAwesomeIcon
+              className="skills__skill__icon"
+              icon={faBootstrap}
+            />
+          </span>
+          <span className="skills__skill" data-name="JavaScript">
+            <FontAwesomeIcon
+              className="skills__skill__icon"
+              icon={faSquareJs}
+            />
+          </span>
+          <span className="skills__skill typescript" data-name="TypeScript">
+            <img className="skills__skill__icon " src={TypeScript} />
+          </span>
+          <span className="skills__skill" data-name="Node.js">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faNodeJs} />
+          </span>
+          <span className="skills__skill" data-name="Vue.js">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faVuejs} />
+          </span>
+          <span className="skills__skill" data-name="React.js">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faReact} />
+          </span>
+          <span className="skills__skill" data-name="git">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faGit} />
+          </span>
+          <span className="skills__skill" data-name="npm">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faNpm} />
+          </span>
+          <span className="skills__skill" data-name="figma">
+            <FontAwesomeIcon className="skills__skill__icon" icon={faFigma} />
+          </span>
         </div>
       </section>
       <hr />
