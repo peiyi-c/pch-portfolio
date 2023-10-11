08bf45fc04a36102b4b25d90473a02ae2ab9b57d
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-11 23:39:33 +0200

    add: contact form

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,10 +5,37 @@ const Footer = () => {
   return (
     <>
       <footer id="contact" className="contact">
-        <h1 className="contact__title">Contact</h1>
-        <div className="contact__links"></div>
+        <h1 className="contact__title">Get In Touch</h1>
+        <form className="contact__form">
+          <ul role="list">
+            <li>
+              <input placeholder="Name" type="text" name="name" required />
+            </li>
+            <li>
+              <input placeholder="Email" type="email" name="email" required />
+            </li>
+            <li>
+              <input
+                placeholder="Subject"
+                type="text"
+                name="subject"
+                required
+              />
+            </li>
+            <li>
+              <textarea
+                placeholder="Message"
+                name="message"
+                required
+              ></textarea>
+            </li>
+            <li>
+              <input type="submit" value="SEND" />
+            </li>
+          </ul>
+          <img className="contact__wave" aria-hidden="true" src={Wave} />
+        </form>
       </footer>
-      <img className="concat__wave" aria-hidden="true" src={Wave} />
     </>
   );
 };
