ac0d5804800f7d4f2bfceaa85beca787ad30ff4d
peiyi-c <pennieyoyoyo@gmail.com>
2023-10-12 17:45:23 +0200

    add: input check

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -16,15 +16,22 @@ const Footer = () => {
       text: "Message successfully sent!",
     },
     failed: {
-      text1: "Please use a valid email.",
-      text2: "Please fill in all required fields!",
-      text3: "Failed to send the message, please try again",
+      text: "Failed to send the message, please try again..",
+    },
+    invalid: {
+      text: "Please check your email!",
+    },
+    incomplete: {
+      text: "Please fill in all required fields!",
     },
     pending: {
-      text: "pending",
+      text: "Pending",
     },
   };
-
+  const checkEmail = (email) => {
+    const regex = new RegExp("[^@]+@[^.]+..+", "i");
+    return email.match(regex);
+  };
   const clearMessage = () => {
     const timer = setTimeout(() => {
       setSend("pending");
@@ -34,6 +41,20 @@ const Footer = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+    if (
+      !refName.current.value.trim() ||
+      !refEmail.current.value.trim() ||
+      !refSubject.current.value.trim() ||
+      !refMessage.current.value.trim()
+    ) {
+      setSend("incomplete");
+      return;
+    }
+    if (!checkEmail(refEmail.current.value)) {
+      setSend("invalid");
+      return;
+    }
 
     emailjs
       .sendForm(
@@ -68,7 +89,12 @@ const Footer = () => {
       <footer id="contact" className="contact">
         <img className="contact__wave-2" aria-hidden="true" src={Wave2} />
         <h1 className="contact__title">Get In Touch</h1>
-        <form ref={refForm} onSubmit={sendEmail} className="contact__form">
+        <form
+          ref={refForm}
+          onSubmit={sendEmail}
+          className="contact__form"
+          noValidate
+        >
           <ul role="list">
             <li>
               <input
@@ -85,6 +111,7 @@ const Footer = () => {
                 placeholder="Email"
                 type="email"
                 name="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
                 required
               />
             </li>
@@ -110,11 +137,7 @@ const Footer = () => {
             </li>
             <li>
               <span className={`${send} contact__form__message`}>
-                {send === "success"
-                  ? status.success.text
-                  : send === "failed"
-                  ? status.failed.text3
-                  : status.pending.text}
+                {status[send]["text"]}
               </span>
             </li>
           </ul>
