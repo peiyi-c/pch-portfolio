d09fa4c18a8745ea9a0e8548ece10b3620828b3d
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-19 18:02:55 +0100

    add project image hover effect

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
-import "./index.scss";
+
 import { useLanguageContext } from "../Language/LanguageContext";
+
 const Project = ({ name, link, image, description }) => {
   const { t } = useLanguageContext();
   return (
@@ -12,11 +13,11 @@ const Project = ({ name, link, image, description }) => {
           rel="noreferrer"
           href={link}
         >
-          <img
+          <div
             className="projects__list__item__image"
-            src={image}
-            alt="project screenshot"
-          />
+            style={{ backgroundImage: `url(${image})` }}
+            role="img"
+          ></div>
         </a>
         <div className="projects__list__item__text">
           <h3 className="projects__list__item__text-name">{name}</h3>
