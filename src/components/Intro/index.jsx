9f457af67488301a675db3e1b48941f9fdc39982
peiyi-c <pennieyoyoyo@gmail.com>
2023-11-19 16:49:47 +0100

    add airship image and animation

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,14 +1,25 @@
 import "./index.scss";
 import { useLanguageContext } from "../Language/LanguageContext";
 import LanguageSelect from "../Language/LanguageSelect";
+import airship from "../../assets/airship.png";
+
 const Intro = () => {
   const { t } = useLanguageContext();
   return (
     <>
       <LanguageSelect />
       <section id="intro" className="intro container">
-        <h1 className="intro__title">Pei-Yi</h1>
-
+        <img
+          className="intro__img"
+          src={airship}
+          alt="airship"
+          role="img"
+          aria-hidden="true"
+        />
+        {/* https://icooon-mono.com/ */}
+        <h1 className="intro__title" role="heading">
+          Pei-Yi
+        </h1>
         <p className="intro__text"> {t("Intro-Text")} </p>
       </section>
     </>
