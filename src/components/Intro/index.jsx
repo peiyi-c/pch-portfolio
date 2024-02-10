3bf4a763d643ce8d6f45ddef069f08f1b77dc554
peiyi-c <pennieyoyoyo@gmail.com>
2024-02-10 11:26:44 +0100

    remove airship

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,7 +1,6 @@
 import "./index.scss";
 import { useLanguageContext } from "../Language/LanguageContext";
 import LanguageSelect from "../Language/LanguageSelect";
-import airship from "../../assets/airship.png";
 
 const Intro = () => {
   const { t } = useLanguageContext();
@@ -9,14 +8,6 @@ const Intro = () => {
     <>
       <LanguageSelect />
       <section id="intro" className="intro container">
-        <img
-          className="intro__img"
-          src={airship}
-          alt="airship"
-          role="img"
-          aria-hidden="true"
-        />
-        {/* https://icooon-mono.com/ */}
         <h1 className="intro__title" role="heading">
           Pei-Yi
         </h1>
